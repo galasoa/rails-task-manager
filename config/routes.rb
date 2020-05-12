551092cbbc1d2426ae0795ad40fba61e18cd9c24
Gala Anania <gsanania@gmail.com>
2020-05-12 17:33:15 +0200

    deleted comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
   resources :tasks
-
-  # get '/tasks',          to: 'tasks#index'
-
-  # get '/tasks/new',      to: 'tasks#new',     as: :new
-  # post 'tasks',          to: 'tasks#create',  as: :create
-
-  # get '/tasks/:id',      to: 'tasks#show',    as: :task
-
-  # get '/tasks/:id/edit', to: 'tasks#edit',    as: :edit
-  # patch '/tasks/:id',    to: 'tasks#update',  as: :update
-
-  # delete '/tasks/:id',   to: 'tasks#destroy', as: :destroy
 end
